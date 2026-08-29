d850b7594e161c201f1a087753a34a0ede454a57
Enaya Laborn <enayalabor@gmail.com>
2026-08-29 14:14:17 -0500

    Update practicetaxform.py

--- IncomeTaxCalculator/practicetaxform.py
+++ IncomeTaxCalculator/practicetaxform.py
@@ -14,7 +14,7 @@ noDep = int(input("Enter the number of dependents: "))
 taxableIncome = grossIncome - STANDARD_DEDUCT - (DEPENDENT_DEDUCT * noDep)
 incomeTax = taxableIncome = grossIncome * TAX_RATE
 
-#Display the income tax (and a small joke to make taxes a little more fun)
+#Display the income tax (and a small joke to make taxes a little more bearable)
 print("The income tax is $" + str(incomeTax) +
       "\nWould you like to pay with cash, check, or wage garnishments?\nPay or go to prison!")
 
